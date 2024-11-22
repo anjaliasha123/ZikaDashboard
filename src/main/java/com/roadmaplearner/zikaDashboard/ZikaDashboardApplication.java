becdcab8319bf66fa843bea40dd51bb54b5cdbbe
anjaliasha123 <anjaliasha95@gmail.com>
2024-11-21 22:32:39 -0600

    initial setup

---
+++ src/main/java/com/roadmaplearner/zikaDashboard/ZikaDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.roadmaplearner.zikaDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZikaDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZikaDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roadmaplearner/zikaDashboard/ZikaDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadmaplearner.zikaDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZikaDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
