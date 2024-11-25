5ff5bd5cb03f80184330f280323b42155989e633
anjaliasha123 <anjaliasha95@gmail.com>
2024-11-25 09:19:16 -0600

    changes

--- src/main/java/com/roadmaplearner/zikaDashboard/controllers/ReportController.java
+++ src/main/java/com/roadmaplearner/zikaDashboard/controllers/ReportController.java
@@ -3,12 +3,14 @@ package com.roadmaplearner.zikaDashboard.controllers;
 import com.roadmaplearner.zikaDashboard.features.FeatureCollection;
 import com.roadmaplearner.zikaDashboard.services.ReportServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/zikaDashboard")
+@CrossOrigin
 public class ReportController {
     @Autowired
     ReportServices reportServices;
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,5 +9,6 @@ CREATE EXTENSION IF NOT EXISTS unaccent;
 --Brazil_Zika-2016-04-02.csv
 COPY report(reported_date, location, location_type, data_field, data_field_code, time_period, time_period_type, value, unit, location_geometry) from '/tmp/Brazil_Zika-2016-04-02.csv' DELIMITER ',' CSV HEADER;
 COPY report(reported_date, location, location_type, data_field, data_field_code, time_period, time_period_type, value, unit, location_geometry) from '/tmp/Panama_Zika-2016-02-18.csv' DELIMITER ',' CSV HEADER;
+COPY report(reported_date, location, location_type, data_field, data_field_code, time_period, time_period_type, value, unit, location_geometry) from '/tmp/Haiti_Zika-2016-02-03.csv' DELIMITER ',' CSV HEADER;
 --COPY report(reported_date, location, location_type, data_field, data_field_code, time_period, time_period_type, value, unit, location_geometry) from '/tmp/MINSA_ZIKA_Search-Week_08.csv' DELIMITER ',' CSV HEADER;
 COMMIT;
