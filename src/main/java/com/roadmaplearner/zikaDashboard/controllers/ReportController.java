2d22bd9f3555aeec9d11d6ef403cc7d09440f45b
anjaliasha123 <anjaliasha95@gmail.com>
2024-11-26 15:24:54 -0600

    keycloak integration

--- src/main/java/com/roadmaplearner/zikaDashboard/controllers/ReportController.java
+++ src/main/java/com/roadmaplearner/zikaDashboard/controllers/ReportController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
 @RequestMapping("/zikaDashboard")
@@ -16,7 +17,13 @@ public class ReportController {
     ReportServices reportServices;
 
     @GetMapping("/")
+//    @PreAuthorize("hasRole('client-zika-user')")
     public FeatureCollection getAllReports() throws Exception {
+        return reportServices.getAllReports();
+    }
+    @GetMapping("/get2")
+//    @PreAuthorize("hasRole('client-zika-admin')")
+    public FeatureCollection getAllReports2() throws Exception {
         return reportServices.getAllReports();
     }
 }
